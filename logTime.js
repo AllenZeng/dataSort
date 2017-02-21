923e0a778731fc08fb39ed3e9bb8f043498e9a4a
AllenZeng <zengjuncm@gmail.com>
2017-02-21 22:51:20 +0800

    🎨：增加排序结果的检测函数

--- logTime.js
+++ logTime.js
@@ -3,7 +3,17 @@ function logTime(fun, sortName, arr) {
 	fun(arr);
 	var endTime = Date.now();
 
-	console.log(sortName, ' 运行时间为：', endTime - startTime, '\n');
+	console.log(sortName, ' 运行时间为：', endTime - startTime);
+	console.log('数组排序结果为正序？：', testArr(arr), '\n');
+}
+
+function testArr(arr) {
+	const isSort = arr.every((val, index, arr) => {
+		if (index === arr.length - 1) return true;
+		else return val <= arr[index + 1];
+	});
+
+	return isSort;
 }
 
 module.exports = logTime;
